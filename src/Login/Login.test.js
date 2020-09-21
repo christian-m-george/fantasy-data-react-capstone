5702bf91076316c1756160838b0139d184818a68
halfpasta <christian.george360@gmail.com>
2020-09-21 14:58:22 -0400

    added smoke tests

---
+++ src/Login/Login.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus, faChevronLeft, faTrashAlt, faCheckDouble
+} from '@fortawesome/free-solid-svg-icons'
+import Login from './Login'
+
+library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>,
+    div
+  )
+  ReactDOM.unmountComponentAtNode(div)
+})
---
+++ src/NavBar/NavBar.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus, faChevronLeft, faTrashAlt, faCheckDouble
+} from '@fortawesome/free-solid-svg-icons'
+import NavBar from './NavBar'
+
+library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <BrowserRouter>
+      <NavBar />
+    </BrowserRouter>,
+    div
+  )
+  ReactDOM.unmountComponentAtNode(div)
+})
